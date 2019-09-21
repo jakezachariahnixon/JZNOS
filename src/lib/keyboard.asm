9ab44f91a695b61b30c0a9de66d489566168a27f
jakezachariahnixon <jake.zachariah.nixon@gmail.com>
2019-09-21 12:45:49 +0100

    lib/keyboard/os_wait_for_key

--- src/lib/keyboard.asm
+++ src/lib/keyboard.asm
@@ -9,3 +9,17 @@
 ; Out:    AX    Key code pressed
 ; Effect:       Waits until a key is pressed and stores the keycode
 ; =============================================================================
+os_wait_for_key:
+    pusha
+
+	mov                ax,             0
+	mov                ah,             10h     ; BIOS call
+	int                16h
+
+	mov                [.os_wait_for_key_tmp_buf],     ax
+
+	popa
+	mov                ax,             [.os_wait_for_key_tmp_buf]
+	ret
+
+    .os_wait_for_key_tmp_buf	dw 0
